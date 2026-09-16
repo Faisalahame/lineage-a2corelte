d4e5083c4018e99487d3053703c966acfab2ce69
Faisalahame <129658684+Faisalahame@users.noreply.github.com>
2026-09-16 22:05:30 +0600

    workflow: build systemimage+bootimage directly, skip broken bacon packaging

--- lineage_a2corelte.mk
+++ lineage_a2corelte.mk
@@ -1,3 +1,4 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/samsung/a2corelte/device.mk)
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
